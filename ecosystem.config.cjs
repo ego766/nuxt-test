2510b904ba36b1c5fa43fcd7c2a7e6bb983329e5
ego766 <745456660@qq.com>
2025-10-06 00:46:20 +0800

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'nuxt-app',
+      name: 'NuxtAppName',
+      port: '3000',
       exec_mode: 'cluster',
-      instances: 1,
+      instances: 'max',
       script: './.output/server/index.mjs',
-      env: {
-        NODE_ENV: 'production',
-        NITRO_PORT: 3000,
-        NITRO_HOST: '0.0.0.0',
-      }
-    }
-  ]
-} 
+    },
+  ],
+}
